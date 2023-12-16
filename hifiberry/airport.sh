2a2da1c9ac52e13daf2b4e2fddbf7e307384a2bc
Pavel Krotkov <pavel.krotkov@gmail.com>
2023-12-16 18:20:23 -0500

    Add initial Bash scripts

---
+++ hifiberry/airport.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Enable error handling, command echoing, and trap for last command on error
+set -e
+set -x
+trap 'echo "Last command executed: $BASH_COMMAND"' ERR
+
+# Update and upgrade packages
+sudo apt-get update
+sudo apt-get -y upgrade
+
+# Install required packages
+sudo apt-get install autoconf automake avahi-daemon build-essential git libasound2-dev libavahi-client-dev libconfig-dev libdaemon-dev libpopt-dev libssl-dev libtool xmltoman
+
+# Clone shairport-sync repository and build it
+git clone https://github.com/mikebrady/shairport-sync.git
+cd shairport-sync
+autoreconf -i -f
+./configure --with-alsa --with-avahi --with-ssl=openssl --with-systemd --with-metadata
+make
+sudo make install
+
+# Start shairport-sync service and enable it to run on startup
+sudo service shairport-sync start
+sudo systemctl enable shairport-sync
+
+# Add configuration to /etc/network/interfaces
+echo "# Disable wifi power management
+wireless-power off" | sudo tee -a /etc/network/interfaces
+
+# Reboot the system
+sudo reboot
+
